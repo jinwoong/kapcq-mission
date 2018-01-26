c1abf9eee085f9a5d05dea5f8c49f3c6917ec5b7
JinwoongJoung <ultraman1128@gmail.com>
2018-01-26 09:34:26 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiKey: "AIzaSyDBKR4Ra6RwlxznrtLxzwaIFlKTXZntYZ0",
-  authDomain: "kapcq-dev.firebaseapp.com",
-  databaseURL: "https://kapcq-dev.firebaseio.com",
-  projectId: "kapcq-dev",
-  storageBucket: "kapcq-dev.appspot.com",
-  messagingSenderId: "628962386546"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
