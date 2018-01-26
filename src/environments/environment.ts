c77676f68df6fc97dc8677b6e9df4fda5e418feb
Jinwoong Joung <jinwoong.joung@abglobal.com>
2018-01-26 09:41:23 -0500

    test gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 
 export const environment = {
   production: false,
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyDBKR4Ra6RwlxznrtLxzwaIFlKTXZntYZ0",
+  authDomain: "kapcq-dev.firebaseapp.com",
+  databaseURL: "https://kapcq-dev.firebaseio.com",
+  projectId: "kapcq-dev",
+  storageBucket: "kapcq-dev.appspot.com",
+  messagingSenderId: "628962386546"
 };
