f9cf7b27e293e226907f489fec7e482bfb758bfd
Jinwoong Joung <jinwoong.joung@abglobal.com>
2018-01-26 09:29:41 -0500

    Added a config in environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { FormsModule } from '@angular/forms';
 
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { MemberManagementComponent } from './member-management/member-management.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -13,12 +15,12 @@ import { AdminComponent } from './admin/admin.component';
 import { ManagementComponent } from './management/management.component';
 
 var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: environment.apiKey,
+  authDomain: environment.authDomain,
+  databaseURL: environment.databaseURL,
+  projectId: environment.projectId,
+  storageBucket: environment.storageBucket,
+  messagingSenderId: environment.messagingSenderId
 };
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,11 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyDBKR4Ra6RwlxznrtLxzwaIFlKTXZntYZ0",
+  authDomain: "kapcq-dev.firebaseapp.com",
+  databaseURL: "https://kapcq-dev.firebaseio.com",
+  projectId: "kapcq-dev",
+  storageBucket: "kapcq-dev.appspot.com",
+  messagingSenderId: "628962386546"
 };
