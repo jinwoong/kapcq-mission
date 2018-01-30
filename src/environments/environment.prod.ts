a3d39997d4c504d7504c3f1ce6100a264f64a9dd
jinwoong <ultraman1128@gmail.com>
2018-01-29 23:30:25 -0500

    prism-dev

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyC9eFXrdrJz2QV3DDXrANP0SURTXeBHUyM",
+  authDomain: "prism-dev-52ca4.firebaseapp.com",
+  databaseURL: "https://prism-dev-52ca4.firebaseio.com",
+  projectId: "prism-dev-52ca4",
+  storageBucket: "prism-dev-52ca4.appspot.com",
+  messagingSenderId: "283779742159"
+};
