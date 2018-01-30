98c5374040ace03eaef2acac25354db3daf0c937
Han Cho <hankyolcho@mail.adelphi.edu>
2018-01-30 13:29:52 -0500

    Finally works. Added announcement framework

--- src/app/prism-routing/prism-routing.module.ts
+++ src/app/prism-routing/prism-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MemberManagementComponent } from '../member-management/member-management.component';
 import { AdminComponent } from '../admin/admin.component';
+import { AnnouncementComponent } from '../announcement/announcement.component';
 
 
 const routes: Routes = [
   { path: 'member-management', component: MemberManagementComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'announcement', component: AnnouncementComponent},
 ];
 
 @NgModule({
